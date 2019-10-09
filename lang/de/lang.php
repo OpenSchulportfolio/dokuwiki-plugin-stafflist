411fd7f204b582f59228d5b61eea5d909bba14a8
Frank Schiebel <frank@linuxmuster.net>
2019-10-09 21:19:01 +0200

    First commit

---
+++ lang/de/lang.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * additional language strings used by plugin
+ *
+ * german version
+ *
+ * @author Gina Haeussge <osd@foosel.net>
+ */
+
+$lang['filename'] = 'Dateiname';
+$lang['filesize'] = 'Dateigröße';
+$lang['lastmodified'] = 'Letzte Änderung';
+$lang['error_nomatch'] = 'Keine Treffer';
+$lang['error_outsidejail'] = 'Zugriff verweigert';
+$lang['edit_files'] = "Dateien bearbeiten";
+$lang['refresh_filelist'] = "Liste aktualisieren";
